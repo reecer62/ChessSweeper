abfe9a18c044b14387d8bd753ecb9964d8943139
Carter Rollins <crollins@lcdrives.com>
2021-04-01 14:26:59 -0400

    Adds rank and file markings

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import Board from "./components/Board.js";
 
 const board = new Board({ selector: "#board", size: "400px" });
-document.getElementById("flipBoard").onclick = () => board.flipBoard();
+document.getElementById("flipBoard").onclick = () => {
+	document.getElementById("boardContainer").classList.toggle("flipped");
+	board.flipBoard();
+};
