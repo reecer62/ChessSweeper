782d382d7bee0c4a36d0b5a2eeff07522e902186
Carter Rollins <crollins@lcdrives.com>
2021-04-01 15:56:48 -0400

    Cleaning tree

--- main.js
+++
@@ -1,4 +0,0 @@
-import Board from "./components/Board.js";
-
-const board = new Board({ selector: "#board", size: "400px" });
-document.getElementById("flipBoard").onclick = () => board.flipBoard(board);
