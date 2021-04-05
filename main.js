ec37a1b63f18b28247aafe7d5c77a286e527508e
Carter Rollins <crollins@lcdrives.com>
2021-04-05 18:04:31 -0400

    Moves mouse events to root

--- main.js
+++ main.js
@@ -4,15 +4,14 @@ import Board from "./components/Board.js";
 const network = new Network({ ip: "173.95.165.30", port: "25568" });
 const board = new Board({ selector: "#board", size: "400px", network, statusCB: (text) => document.getElementById("status").innerHTML = text });
 
-let body = document.getElementById("body");
-body.onmousedown = (event) => {
+document.onmousedown = (event) => {
 	board.mouseDown(event);
 	return false;
 }
-body.onmouseup = (event) => {
+document.onmouseup = (event) => {
 	board.mouseUp(event);
 }
-body.onmousemove = (event) => {
+document.onmousemove = (event) => {
 	board.mouseMove(event);
 }
 
